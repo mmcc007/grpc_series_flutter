d135c404233553c0d7968cfb5c9ea56dad5632f5
Maurice McCabe <mmcc007@gmail.com>
2020-09-01 15:47:47 -0700

    Remove unused directory

--- util/protoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-(
-    cd flutter_app
-    generated='lib/src/generated'
-    rm -rf $generated
-    mkdir -p $generated
-    protoc --dart_out=grpc:$generated -Iproto /usr/local/Cellar/protobuf/3.12.4/include/google/protobuf/*.proto proto/GreetingService.proto
-
-)
